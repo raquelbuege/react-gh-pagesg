82de60bce0f948bca588c9a1f98a12541e6e90e1
rachelbuege <121844475+rachelbuege@users.noreply.github.com>
2024-04-06 22:54:08 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Editttt <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LEARN
-        </a>
       </header>
     </div>
   );
